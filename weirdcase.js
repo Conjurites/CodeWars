aec06ef65e10ab1da768c17aafb74aa00d031eee
Conjurites <72075808+Conjurites@users.noreply.github.com>
2020-09-29 14:34:07 +0100

    WeIrD StRiNg CaSe Challenge

---
+++ weirdcase.js
@@ -0,0 +1,7 @@
+toWeirdCase = (string) => {
+  return string.split(' ').map((word) => {
+    return word.split('').map((letter, index) => {
+      return index % 2 == 0 ? letter.toUpperCase() : letter.toLowerCase()     
+    }).join('');
+  }).join(' ');
+}
